5cd6d6adf0f49f010ddf2dfbfb5a14981e5525bd
iskandar <admiskandar@gmail.com>
2024-03-07 11:39:48 +0800

    add new default user @ DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,13 +23,33 @@ class DatabaseSeeder extends Seeder
                 'user_type' => '1',
             ],
             [
-                'name' => 'Kiosk',
-                'email' => 'kiosk@orderlikey.com',
+                'name' => 'Kiosk-1',
+                'email' => 'kiosk-1@orderlikey.com',
                 'user_type' => '2',
             ],
             [
-                'name' => 'Customer',
-                'email' => 'customer@orderlikey.com',
+                'name' => 'Kiosk-2',
+                'email' => 'kiosk-2@orderlikey.com',
+                'user_type' => '2',
+            ],
+            [
+                'name' => 'Kiosk-3',
+                'email' => 'kiosk-3@orderlikey.com',
+                'user_type' => '2',
+            ],
+            [
+                'name' => 'Customer-1',
+                'email' => 'customer-1@orderlikey.com',
+                'user_type' => '3',
+            ],
+            [
+                'name' => 'Customer-2',
+                'email' => 'customer-2@orderlikey.com',
+                'user_type' => '3',
+            ],
+            [
+                'name' => 'Customer-3',
+                'email' => 'customer-3@orderlikey.com',
                 'user_type' => '3',
             ],
         ]);
